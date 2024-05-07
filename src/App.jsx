079f8af74e6ee8be84d7af6e814a9f0ae0299756
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-05-07 10:53:01 +0100

    adds list of all articles with basic responsive styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./css/ArticleCard.css";
 import { Routes, Route } from "react-router-dom";
 import Articles from "./components/Articles";
 import Article from "./components/Article";
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,1 +1,23 @@
-export default function Articles() {}
+import { useEffect, useState } from "react";
+import { allArticles } from "../api";
+import ArticleCard from "./ArticleCard";
+
+export default function Articles() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    allArticles.then((articles) => {
+      setArticles(articles.data.articles);
+    });
+  });
+
+  return (
+    <section>
+      <div className="flex-container">
+        {articles.map((article) => {
+          return <ArticleCard key={article.article_id} article={article} />;
+        })}
+      </div>
+    </section>
+  );
+}
