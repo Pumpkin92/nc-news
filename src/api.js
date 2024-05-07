a8df06125a13151290cef78251444b5ed524206e
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-05-07 16:21:28 +0100

    adds comment and article voting functionality

--- src/api.js
+++ src/api.js
@@ -26,4 +26,40 @@ const getCommentsByArticleId = (article_id) => {
     });
 };
 
-export { getAllArticles, getArticleById, getCommentsByArticleId };
+const patchCommentVote = (comment_id, vote) => {
+  const votes = { inc_votes: vote };
+  return axios
+    .patch(
+      `https://nc-news-project-q2e5.onrender.com/api/comments/${comment_id}`,
+      votes
+    )
+    .then((response) => {
+      return response;
+    });
+};
+
+const patchArticleVote = (article_id, vote) => {
+  const votes = { inc_votes: vote };
+  return axios
+    .patch(
+      `https://nc-news-project-q2e5.onrender.com/api/articles/${article_id}`,
+      votes
+    )
+    .then((response) => {
+      if (response.status !== 200) {
+        return Promise.reject({
+          status: response.status,
+          msg: response.statusText,
+        });
+      }
+      return response;
+    });
+};
+
+export {
+  getAllArticles,
+  getArticleById,
+  getCommentsByArticleId,
+  patchCommentVote,
+  patchArticleVote,
+};
