46c9c2e28327b22dc556093470eaaa605724cf7e
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2024-07-01 13:25:09 +0100

    updates some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,18 @@ import { faMugHot } from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
   return (
     <header>
-      <div className="header-div">
-        <h1>NC News</h1>
-        <div className="header-logo-div">
-          <FontAwesomeIcon
-            icon={faMugHot}
-            style={{ color: "purple" }}
-            className="fa-3x"
-          />
+      <Link to="/">
+        <div className="header-div">
+          <h1>NC News</h1>
+          <div className="header-logo-div">
+            <FontAwesomeIcon
+              icon={faMugHot}
+              style={{ color: "#836faa" }}
+              className="fa-3x"
+            />
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="home-div">
         <Link to="/">Home</Link>
       </div>
